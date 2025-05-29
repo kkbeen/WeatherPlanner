8846bd67aa05c22362857ef0c3e73588c5062c7f
hyungyu-02 <yona20020209@gmail.com>
2025-05-29 16:39:59 +0900

    feat: TopAppBar 생성

--- app/src/main/java/com/example/weatherplanner/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/weatherplanner/ui/home/HomeScreen.kt
@@ -1,13 +1,54 @@
 package com.example.weatherplanner.ui.home
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.weatherplanner.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Text(text = "Home Screen")
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        TopAppBar(
+            modifier = Modifier.fillMaxWidth(),
+            title = {
+                Icon(
+                    modifier = Modifier.size(30.dp),
+                    painter = painterResource(R.drawable.weather_planner_logo),
+                    contentDescription = "weatherplanner logo",
+                    tint = Color.Unspecified
+                )
+            },
+            actions = {
+                Row(
+                    modifier = Modifier.padding(20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(id = R.drawable.ic_home_bell),
+                        contentDescription = "home bell icon",
+                        tint = Color.Unspecified
+                    )
+                }
+            }
+        )
+    }
 }
 
 @Preview
